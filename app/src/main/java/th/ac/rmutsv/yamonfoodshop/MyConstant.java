b927ee4e788a512f8a6e685e21b37f76e434ded9
yamon <sayamon.bud@gmail.com>
2019-05-08 15:38:00 +0700

    Read JSON Success

--- app/src/main/java/th/ac/rmutsv/yamonfoodshop/MyConstant.java
+++ app/src/main/java/th/ac/rmutsv/yamonfoodshop/MyConstant.java
@@ -5,8 +5,14 @@ public class MyConstant {
 
 
     private String urlAddUser = "https://www.androidthai.in.th/ruts/addUserMon.php";
+    private String urlGetUserWhereUser = "https://www.androidthai.in.th/ruts/getUserWhereUserMon.php";
+
+    public String getUrlGetUserWhereUser() {
+        return urlGetUserWhereUser;
+    }
 
     public String getUrlAddUser() {
+
         return urlAddUser;
     }
 }
